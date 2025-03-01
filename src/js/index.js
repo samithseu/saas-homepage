463172154bf2f08e82b55af8acf54b7da29da073
samithseu <samithseu2002@gmail.com>
2025-03-01 19:10:57 +0700

    created header, hero, features, testimonials

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+import "../css/style.css";
+
+const navlink = document.querySelector("#navlink");
+const cta = document.querySelector("#cta");
+const toggle = document.querySelector("#toggle");
+const headerNav = document.querySelector("header > nav");
+
+// hide menu first for mobile
+if (window.innerWidth < 768) {
+  navlink.classList.add("hidden");
+  cta.classList.add("hidden");
+}
+
+// bring back for desktop
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    navlink.classList.remove("hidden");
+    cta.classList.remove("hidden");
+  } else {
+    navlink.classList.add("hidden");
+    cta.classList.add("hidden");
+  }
+});
+
+// burger menu
+toggle.addEventListener("click", (e) => {
+  if (window.innerWidth < 768) {
+    navlink.classList.toggle("hidden");
+    cta.classList.toggle("hidden");
+  }
+});
+
+// change the padding of headerNav when scrolling down a bit
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    headerNav.classList.remove("py-6");
+    headerNav.classList.add("py-4");
+  } else {
+    headerNav.classList.add("py-6");
+  }
+});
