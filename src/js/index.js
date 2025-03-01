652747941ab83cd78ebafe838ca86c4431b48bbb
samithseu <samithseu2002@gmail.com>
2025-03-01 23:43:56 +0700

    added a `IntersectionObserver` all some elements

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const navlink = document.querySelector("#navlink");
 const cta = document.querySelector("#cta");
 const toggle = document.querySelector("#toggle");
 const headerNav = document.querySelector("header > nav");
+const allCards = document.querySelectorAll("h1,h2,h3,h4,div");
 
 // hide menu first for mobile
 if (window.innerWidth < 768) {
@@ -36,8 +37,25 @@ window.addEventListener("scroll", () => {
     headerNav.classList.remove("py-6");
     headerNav.classList.add("py-4");
     headerNav.parentElement.classList.add("bg-white/60");
+    headerNav.parentElement.classList.add("shadow-sm");
   } else {
     headerNav.classList.add("py-6");
     headerNav.parentElement.classList.remove("bg-white/60");
+    headerNav.parentElement.classList.remove("shadow-sm");
   }
 });
+
+// appear on scroll
+const observer = new IntersectionObserver(
+  (entries, obs) => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        entry.target.style.setProperty("--delay", `${index * 50}ms`); // Set delay dynamically
+        entry.target.classList.add("scroll-show");
+        obs.unobserve(entry.target); // Unobserve to improve performance
+      }
+    });
+  },
+  { threshold: 0.15 }
+);
+allCards.forEach((el) => observer.observe(el));
